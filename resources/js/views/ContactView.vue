8ff84b1659231b59afd78ee6dfcb5fe8f885b42e
cm-hamza <hamza@claimsmedinc.com>
2024-06-21 07:42:45 -0700

    first commit

--- resources/js/views/ContactView.vue
+++ resources/js/views/ContactView.vue
@@ -24,10 +24,23 @@
             </form>
             <div class="mt-4">
                 <h3>My Inquiries</h3>
-                
+                <div v-for="contact in contacts" :key="contact.id" >
+                    <div class="contact">
+                    <h4>{{ contact.name }} </h4>
+                    </div>
+                </div>
             </div>
         </div>
 </template>
+
+<style>
+.contact{
+    background-color: grey;
+    padding: 10px;
+    margin: 10px;
+}
+
+</style>
 
 <script>
 import axios from 'axios';
@@ -38,7 +51,11 @@ export default {
             email:null,
             number:null,
             message:null,
+            contacts:[],
         }
+    },
+    mounted(){
+        this.getContacts();
     },
     methods: {
         async submitdata(e){
@@ -58,8 +75,18 @@ export default {
                 alert('submiteed')
             }
             console.log(submitData);
+        },
+        async getContacts(){
+            try {
+                const response = await axios.get('/fetch/contact');
+                this.contacts = response.data;
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+    
+            }
         }
     },
 
 }
 </script>
+
